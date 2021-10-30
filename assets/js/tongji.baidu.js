97e28f72ff2f80d02f5d4323580b71565777107e
Passer <49974679+ChenChenfei@users.noreply.github.com>
2021-10-30 19:35:22 +0800

    Add files via upload

---
+++ assets/js/tongji.baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ec7470452f344e57c8085be5be58583f";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
